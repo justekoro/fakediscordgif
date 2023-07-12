8dbc162d435bb4c8241a2ddcfe61837cb50f7cb0
koro <me@krbk.dev>
2023-07-12 17:55:41 +0200

    :bug: Change url to /gif.gif

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const image = fs.readFileSync("./image.gif");
 
-app.all("/notavirus.gif", async (req, res) => {
+app.all("/gif.gif", async (req, res) => {
     if (req.headers["user-agent"].includes("bot") || req.headers["user-agent"] === "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0") {
         res.header("Content-Type", "image/gif").send(image);
     }
